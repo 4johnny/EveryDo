5d7bcd88ecb80b86cc4fa25fbade67145d8f9ed0
4johnny <4johnny@users.noreply.github.com>
2015-02-04 23:40:48 -0800

    Minor clean.

--- EveryDo/MasterViewController.m
+++ EveryDo/MasterViewController.m
@@ -62,7 +62,7 @@
 	//	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
 	//	self.navigationItem.rightBarButtonItem = addButton;
 	
-	self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+	//	self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
 
